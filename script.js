2f883258351a451aa7b2abfc57ed391603942168
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-10 11:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
-
+    // alert("button clicked!");
+	const firstname = document.querySelector(".fname").value.trim();
+		const lastname = document.querySelector(".lname").value.trim();
+  if(firstname==="" || lastname===""){
+	  alert("all fields are required:");
+	  return;
+  }
+	alert(`${firstname} ${lastname}`);
 }
